f60c3a83fdf6e77b0695e02c423bc7594c8be456
AndreyGRE <andrey.grenev@mail.ru>
2022-10-15 20:00:27 +0300

    u1

--- src/App.js
+++ src/App.js
@@ -11,27 +11,25 @@ function id2() {
 	return uuid();
 };
 
-function App() {
-	const [notes, setNotes] = useState(['a', 'b', 'c', 'd', 'e']);
-	const [value, setValue] = useState('');
-	
-	const result = notes.map((note, index) => {
-		return <li key={index}>{note}</li>;
-	});
-	function f1 (event) {
-		setNotes([...notes, value]);
+//Выведите элементы этого массива в виде списка ul. 
+//Под списком реализуйте инпут для редактирования пунктов списка. 
+//Пусть в конце каждой li стоит кнопка, по нажатию на которую будет начинаться редактирование этой li.
 
-	alert (event.target.value);
-	console.log (event.target.value);
-	event.target.value = '';
-	console.log (event.target.value);
+function App() {
+	const [obj, setObj] = useState(initObj);
+	
+	function handleChange(prop, event) {
+		setObj({...obj, ...{[prop]: event.target.value}});
 	}
-	return <ul>
-		{result}
+	
+	return <div>
+		<input value={obj.prop1} onChange={event => handleChange('prop1', event)} />
+		<input value={obj.prop2} onChange={event => handleChange('prop2', event)} />
+		<input value={obj.prop3} onChange={event => handleChange('prop3', event)} />
 		
-		<input value={value} onChange={event => setValue(event.target.value)} onBlur={f1}/>
-		
-	</ul>;
+		<br />
+		{obj.prop1}-{obj.prop2}-{obj.prop3}
+	</div>;
 }
 
 export default App;

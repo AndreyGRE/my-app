cebf2f2e18b0b272c570bf9027a3776319f84b26
AndreyGRE <andrey.grenev@mail.ru>
2022-10-15 17:20:19 +0300

    1
    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react'; // стейты делает 
+import { nanoid } from 'nanoid/non-secure' //id создает 
+import uuid from 'react-uuid';//id создает 
+
+function id() {
+	return nanoid(); //=> "Uakgb_J5m9g-0JDMbcJqLJ" ;
+	//const id = nanoid() //=> "Uakgb_J5m9g-0JDMbcJqLJ"
+};
+function id2() {
+	return uuid();
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [notes, setNotes] = useState(['a', 'b', 'c', 'd', 'e']);
+	const [value, setValue] = useState('');
+	
+	const result = notes.map((note, index) => {
+		return <li key={index}>{note}</li>;
+	});
+	function f1 (event) {
+		setNotes([...notes, value]);
+
+	alert (event.target.value);
+	console.log (event.target.value);
+	event.target.value = '';
+	console.log (event.target.value);
+	}
+	return <ul>
+		{result}
+		
+		<input value={value} onChange={event => setValue(event.target.value)} onBlur={f1}/>
+		
+	</ul>;
 }
 
 export default App;
+
